a4d5ff8acc73610eb32741e8d33c3b013b15fe95
ardianto nugroho <ardianto.nugroho@dispendix.com>
2023-07-15 00:02:01 +0700

    refactor: remove hello world

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -13,8 +12,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
-
 	var configFileName string
 	flag.StringVar(&configFileName, "c", "config.yml", "Config file name")
 
